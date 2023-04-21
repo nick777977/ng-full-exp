1fd6b8d8c4ba8f359538e1de62b1a94a93178bdd
nick-srctree-epam <nick.push777977@gmail.com>
2023-04-21 21:24:56 +0300

    hot using multicast() .refCount()+Subject factory

--- rxjs/hot-cold-warm-observables/hot.mjs
+++ rxjs/hot-cold-warm-observables/hot.mjs
@@ -3,6 +3,7 @@ import { multicast, of, defer, Subject } from "rxjs";
 
 // Make hot using multicast() and connect() / .refCount()
 // multicast() !!! deprecated
+console.log('1--------------------');
 let random = () => Math.floor(Math.random() * 100);
 
 let cold = defer(() => of(random()));
@@ -11,7 +12,7 @@ let hot = cold.pipe(multicast(new Subject())); // Subject as a proxy between pro
 hot.subscribe((x) => console.log("1:", x));
 hot.subscribe((x) => console.log("2:", x));
 
-//hot.connect(); // creates connection between Subject and Provider. !!! without connect() subs will not get any values
+hot.connect();
 
 /*  output:
     1: 83
@@ -21,9 +22,10 @@ hot.subscribe((x) => console.log("2:", x));
 
 // Make hot using multicast() .refCount()
 // multicast() !!! deprecated
+console.log('2--------------------');
 let random2 = () => Math.floor(Math.random() * 100);
 
-let cold2 = defer(() => of(random())); // this is the source(Producer)
+let cold2 = defer(() => of(random2())); // this is the source(Producer)
 let hot2 = cold2.pipe(multicast(new Subject())).refCount(); // with refCount you don't need to use hot.connect(). refCount will count all subs. When we have at least 1 sub we will start to get the values, and right after it will end the the stream, because our source will be executed only once.
 
 hot2.subscribe((x) => console.log("1:", x));
@@ -32,4 +34,22 @@ hot2.subscribe((x) => console.log("2:", x));
 
 /*  output:
     1: 18
+*/
+
+
+// Make hot using multicast() .refCount() + Subject factory
+// multicast() !!! deprecated
+console.log('3--------------------');
+let random3 = () => Math.floor(Math.random() * 100);
+
+let cold3 = defer(() => of(random3()));
+let hot3 = cold3.pipe(multicast(() => new Subject())).refCount(); // this Subject factory will generate a new Subject for each new sub
+
+hot3.subscribe((x) => console.log("1:", x));
+hot3.subscribe((x) => console.log("2:", x));
+
+
+/*  output:
+    1: 63
+    2: 20
 */

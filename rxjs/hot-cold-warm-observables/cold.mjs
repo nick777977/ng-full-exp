86bfb6764d1882bb150560b30623fdbe8f8e5fb1
nick-srctree-epam <nick.push777977@gmail.com>
2023-04-21 17:58:53 +0300

    add ReplaySubject

--- rxjs/hot-cold-warm-observables/cold.mjs
+++ rxjs/hot-cold-warm-observables/cold.mjs
@@ -3,7 +3,9 @@
 */
 
 
-import { Subject } from 'rxjs';
+import { ReplaySubject, Subject } from 'rxjs';
+
+// regular Subject
 let subj = new Subject();
 subj.subscribe(
   (val) => console.log('first next:', val),
@@ -21,5 +23,35 @@ subj.subscribe(
 /* output:
     first next: Yes, I can
     first complete
+    second complete
+ */
+
+// ReplaySubject
+// ReplaySubject will repeat the last emitted value to all new subscribers
+// ReplaySubject constructor can specify the number of last emitted values to repeat (buffer size). !!! default buffer size to infinite
+
+let replaySubj = new ReplaySubject(1);
+replaySubj.subscribe(
+  val => console.log('first next:', val),
+  err => console.log('first was error:', err),
+  () => console.log('first complete')
+);
+
+replaySubj.next( Math.floor((Math.random() * 100)) );
+replaySubj.next( Math.floor((Math.random() * 100)) );
+replaySubj.complete();
+
+replaySubj.subscribe(
+  val => console.log('second next:', val),
+  err => console.log('second was error:', err),
+  () => console.log('second complete')
+);
+
+
+/* output:
+    first next: 85
+    first next: 77
+    first complete
+    second next: 77
     second complete
  */
